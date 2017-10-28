f04af537e41f6aef0742fdeed9c67c4455dd290b
Brwnber <kylebsmall32@gmail.com>
2017-10-27 23:21:54 -0400

    Mostly Done
    I have it spitting out a string and the number, but I need to find a way to stop the while loop once it finds the largest number. Lots of googling

--- largestNumber.js
+++ largestNumber.js
@@ -1,2 +1,24 @@
-let numArray = [10, 32, 8, 60, 12, 5, 40, 54, 21, 28]
+let numArray = [10, 32, 7, 60, 12, 9, 40, 54, 21, 100];
 
+let largest = 0; 
+
+for (i = 0; i < numArray.length; i++) {
+    let current = numArray[i]
+
+    if (current > largest) {
+        largest = current;
+    }
+}
+
+let j = 0
+
+while (j < numArray.length) {
+    let current = numArray[j]
+    console.log("The greatest number is: " + largest)
+    j++
+
+    if (current > largest) {
+        largest = current;
+    }
+}
+console.log(largest)
